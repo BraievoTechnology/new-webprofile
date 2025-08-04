24625457ec294edd9e626f365282a82eae5d9749
Saminda Uthpala <120486850+Sam1ndaFernando@users.noreply.github.com>
2025-08-04 20:27:29 +0530

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ['Sora', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'], 
+      },
+    },
+  },
+  plugins: [],
+}
